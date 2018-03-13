7d6a079b7f62d877f28ef0172e66b5b2ea74ed66
luigi1997 <luigi1997@live.com.pt>
2018-03-13 16:36:17 +0000

    wanted commit

---
+++ PP/FIcha 4/JavaApplication56_Caterpie/src/javaapplication56_caterpie/User.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication56_caterpie;
+
+/**
+ *
+ * @author Luís Pereira
+ */
+public class User {
+
+    private final int ID_SIZE = 3;
+    protected char[] id = new char[ID_SIZE];
+    protected char[] name;
+    protected char[] email;
+    
+    public User(){
+        
+    }
+}
